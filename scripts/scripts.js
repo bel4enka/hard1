4b0c117fa09ed2d698224146a1efd377756ea61d
Оксана Красильникова <oxidjini@yandex.ru>
2021-10-31 23:49:58 +0300

    Тёмная тема

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,37 @@
 const switchTheme = document.querySelector('.switch-theme');
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
 
 /**
- * Переключатель темы. Само изменение темы ещё не работает.
+ * Открыть/Закрыть меню
+ */
+function toggleMenu () {
+    document.querySelector('.nav').classList.toggle('nav_opened')
+    document.querySelector('.menu').classList.toggle('menu_opened')
+
+    /**
+     * Закрыть меню при скролле
+     */
+    window.addEventListener('scroll', function () {
+        nav.classList.remove('nav_opened')
+        menu.classList.remove('menu_opened')
+    })
+}
+
+/**
+ * Переключатель темы
  */
 function toggleSwitchTheme () {
     switchTheme.classList.toggle('switch-theme_dark')
-    document.querySelector('.body').classList.toggle('body_theme_dark')
+    document.querySelector('.body').classList.toggle('theme-dark')
+    document.querySelector('.body').classList.toggle('theme-light')
 }
 
 /**
  * Отрытие меню
  */
 document.querySelector('.nav__wrap').addEventListener('click', function () {
-    document.querySelector('.nav').classList.toggle('nav_opened')
-    document.querySelector('.menu').classList.toggle('menu_opened')
+    toggleMenu()
 })
 
 
@@ -34,5 +52,6 @@ for (let anchor of anchors) {
         })
     })
 }
+
 
 switchTheme.addEventListener('click', toggleSwitchTheme)
